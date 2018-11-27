e39e3190be652c673edbad48acaf0aaba0b592f7
energyd <energyd@users.noreply.github.com>
2018-11-26 23:17:28 -0800

    Update Ten_Dji.java

--- Ten_Dji.java
+++ Ten_Dji.java
@@ -1,3 +1,53 @@
+import java.util.*;
+
+public class Ten {
+
+  public static void main(String[] args) {
+    List<List<Integer>> graph = new ArrayList<>();
+    for (int i = 0; i <= 9; i++) {
+      graph.add(new ArrayList<>());
+    }
+
+    // 0: 1,3,9
+    // 3: 5,9
+    // 4: 6
+    // 5: 6,9,4
+    // 7: 5
+    // 9: 4
+    graph.get(0).add(1);
+    graph.get(0).add(9);
+    graph.get(0).add(3);
+    graph.get(3).add(5);
+    graph.get(3).add(9);
+    graph.get(4).add(6);
+    graph.get(5).add(4);
+    graph.get(5).add(6);
+    graph.get(5).add(9);
+    graph.get(7).add(5);
+    graph.get(9).add(4);
+    Ten sol = new Ten();
+    List<Integer> res = sol.getShortestPath(graph, 0 , 9);
+    System.out.println("min cost: " + sol.min);
+    for (int i : res) {
+      System.out.print(i + ", ");
+    }
+  }
+
+  class Wizard implements Comparable<Wizard> {
+    int cost;
+    int id;
+    Wizard parent;
+    public Wizard(int id, int cost) {
+      this.id = id;
+      this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Wizard w) {
+      return Integer.compare(this.cost, w.cost);
+    }
+  }
+
   int min = Integer.MAX_VALUE;
   public List<Integer> getShortestPath(List<List<Integer>> wizards, int src, int target) {
     Wizard end = null;
@@ -35,3 +85,4 @@
     }
     return res;
   }
+}
